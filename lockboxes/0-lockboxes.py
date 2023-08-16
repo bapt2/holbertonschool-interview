e3d9a3168990b5ac48bba25f2887b2ed3b6f9e23
bapt2 <113673417+bapt2@users.noreply.github.com>
2023-08-16 10:25:02 +0200

    adding lock boxe

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    boxeLen = len(boxes)
+    key = [0]
+    
+    for i in key:
+        if i < boxeLen:
+            for j in boxes[i]:
+                if j not in key and j < boxeLen:
+                    key.append(j)
+    if len(key) == boxeLen:
+        return True
+    return False
