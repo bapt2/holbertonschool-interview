a26ac17b1e5dad59d35766b51bc3d3421ab94e98
bapt2 <baptiste18c.touraud@gmail.com>
2023-09-14 20:36:24 +0200

    fixing betty style

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -7,56 +7,56 @@
  */
  int is_stable(int grid[3][3])
  {
-    int i, j;
+int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if(grid[i][j] > 3)
-            {
-                return 1;
-            }
-        }
-    }
-    return 0;
- }
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 3; j++)
+{
+if(grid[i][j] > 3)
+{
+return 1;
+}
+}
+}
+return 0;
+}
 
 /**
  * topple - topple a sandpiles
  * @grid: grid to topple
  */
- void topple(int grid[3][3])
- {
-    int i, j;
+void topple(int grid[3][3])
+{
+int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid[i][j] > 3)
-            {
-                grid[i][j] -= 4;
-                if (i - 1 >= 0)
-                {
-                    grid[i - 1][j]++;
-                }
-                if (i + 1 < 3)
-                {
-                    grid[i + 1][j]++;
-                }
-                if (j - 1 >= 0)
-                {
-                    grid[i][j - 1]++;
-                }
-                if (j + 1 < 3)
-                {
-                    grid[i][j + 1]++;
-                }
-            }
-        }
-    }
- }
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 3; j++)
+{
+if (grid[i][j] > 3)
+{
+grid[i][j] -= 4;
+if (i - 1 >= 0)
+{
+grid[i - 1][j]++;
+}
+if (i + 1 < 3)
+{
+grid[i + 1][j]++;
+}
+if (j - 1 >= 0)
+{
+grid[i][j - 1]++;
+}
+if (j + 1 < 3)
+{
+grid[i][j + 1]++;
+}
+}
+}
+}
+}
 
 /**
  * sandpiles_sum - function that computes the sum of two sandpiles
@@ -65,31 +65,31 @@
  **/
  void sandpiles_sum(int grid1[3][3], int grid2[3][3])
  {
-    int i, j;
+int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            grid1[i][j] += grid2[i][j];
-        }
-    }
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 3; j++)
+{
+grid1[i][j] += grid2[i][j];
+}
+}
 
-    while(is_stable(grid1))
-    {
-        printf("=\n");
-        for (i = 0; i < 3; i++)
-        {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-            {
-                printf(" ");
-            }
-            printf("%d", grid1[i][j]);
-        }
-        printf("\n");
-    }
-    topple(grid1);
-    }
- }
+while(is_stable(grid1))
+{
+printf("=\n");
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 3; j++)
+{
+if (j)
+{
+printf(" ");
+}
+printf("%d", grid1[i][j]);
+}
+printf("\n");
+}
+topple(grid1);
+}
+}
